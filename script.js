c93147a8107e5075ca8a5199a6720a9f5b08569a
MinaMalak-cmd <engmina15@yahoo.com>
2021-12-03 00:44:52 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var tag = document.createElement("p");
+var text = document.createTextNode("First comment");
+tag.appendChild(text);
+var element = document.querySelector("body");
+element.appendChild(tag);
+//Firebase token : 1//03a5-UhMhNN-zCgYIARAAGAMSNwF-L9IrWuJK7Js7QXE1gu3AHQLkB0jkyeu8l7oqF2CeU3xzTOnsDCcnDvhxTJ2UsteZY7jU_ZM
