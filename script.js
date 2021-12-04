73c2320fda4a22c7a6e42770eb61dbbd3b8b19e8
MinaMalak-cmd <59808870+MinaMalak-cmd@users.noreply.github.com>
2021-12-04 16:11:03 +0200

    add react project

--- script.js
+++
@@ -1,9 +0,0 @@
-var tag = document.createElement("p");
-var text = document.createTextNode("First comment");
-tag.appendChild(text);
-var element = document.querySelector("body");
-element.appendChild(tag);
-//Firebase token : 1//03a5-UhMhNN-zCgYIARAAGAMSNwF-L9IrWuJK7Js7QXE1gu3AHQLkB0jkyeu8l7oqF2CeU3xzTOnsDCcnDvhxTJ2UsteZY7jU_ZM
-
-var tag = document.createElement("h3");
-var text = document.createTextNode("Second comment");
