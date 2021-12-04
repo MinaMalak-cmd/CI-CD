73e131869c2cdd465107b0e7e1f6fc13a56f3a82
MinaMalak-cmd <59808870+MinaMalak-cmd@users.noreply.github.com>
2021-12-04 15:08:38 +0200

    add new element

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ tag.appendChild(text);
 var element = document.querySelector("body");
 element.appendChild(tag);
 //Firebase token : 1//03a5-UhMhNN-zCgYIARAAGAMSNwF-L9IrWuJK7Js7QXE1gu3AHQLkB0jkyeu8l7oqF2CeU3xzTOnsDCcnDvhxTJ2UsteZY7jU_ZM
+
+var tag = document.createElement("h3");
+var text = document.createTextNode("Second comment");
