9cc303649995d6a7d8cd8a798525ecf807fbea75
MinaMalak-cmd <59808870+MinaMalak-cmd@users.noreply.github.com>
2021-12-04 16:30:27 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Board from "./Board";
-import { updateURLParameter } from "./helpers";
+import React from "react";
 
 function App() {
-  const [imgUrl, setImgUrl] = useState("");
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has("img")) {
-      setImgUrl(urlParams.get("img"));
-    }
-  }, []);
-
-  const handleImageChange = (e) => {
-    setImgUrl(e.target.value);
-    window.history.replaceState(
-      "",
-      "",
-      updateURLParameter(window.location.href, "img", e.target.value)
-    );
-  };
-
   return (
     <div className="App">
-      <h1>React sliding puzzle</h1>
-      <Board imgUrl={imgUrl} />
-      <input value={imgUrl} onChange={handleImageChange} />
+      <h1>React sliding puzzle</h1>  
+      <h2>CI/CD</h2>   
     </div>
   );
 }
